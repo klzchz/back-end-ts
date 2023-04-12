28d098205f60c9e66f8370b29bb1c522e5df5ce1
Lucas Andrade <lucascorporation@live.com>
2023-04-11 22:01:48 -0300

    subindo servidor com express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('started');
+import dotenv from 'dotenv';
+import express, { response } from 'express';
+
+dotenv.config()
+
+const PORT = process.env.port || 8080;
+
+const app = express();
+
+app.get('/',(req,res)=>{
+
+  res.send("Server Up a");
+
+});
+
+app.listen(PORT,()=>{
+  console.log(`Server Runing  in port ${PORT}`);
+});
+
